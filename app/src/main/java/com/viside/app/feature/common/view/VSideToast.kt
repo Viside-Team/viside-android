da5c67dfcd65f09dd183f615da56012caa4ba7db
depayse <cbs5295@gmail.com>
2022-10-01 15:03:28 +0900

    [FEAT] 토스트 메세지 뷰 및 클래스 작업

---
+++ app/src/main/java/com/viside/app/feature/common/view/VSideToast.kt
@@ -0,0 +1,30 @@
+package com.viside.app.feature.common.view
+
+import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.viside.app.databinding.LayoutToastBinding
+
+object VSideToast {
+    private fun createShortToast(context: Context, message: String): Toast {
+        val inflater = LayoutInflater.from(context)
+        val binding: LayoutToastBinding = LayoutToastBinding.inflate(inflater).apply {
+            tvToastText.text = message
+        }
+
+        return Toast(context).apply {
+            duration = Toast.LENGTH_SHORT
+            view = binding.root
+        }
+    }
+
+    fun createShortCenterToast(context: Context, message: String): Toast =
+        createShortToast(context, message).apply {
+            setGravity(Gravity.CENTER, 0, 0)
+        }
+
+
+    fun createShortCenterHorizontalToast(context: Context, message: String): Toast =
+        createShortToast(context, message)
+}
