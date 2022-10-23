aa73fd9a19c506262b3583fb5522539651438038
depayse <cbs5295@gmail.com>
2022-10-23 16:13:59 +0900

    [FEAT] 메인 탭 아이템 태그 투명도 설정

--- app/src/main/java/com/vside/app/util/common/CommonBindingAdapter.kt
+++ app/src/main/java/com/vside/app/util/common/CommonBindingAdapter.kt
@@ -1,1 +1,22 @@
 package com.vside.app.util.common
+
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import androidx.cardview.widget.CardView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("bgColorAlpha")
+fun View.setBgColorAlpha(alpha0To100: Int?) {
+    alpha0To100?.let {
+        if(background is ColorDrawable) {
+            (background as ColorDrawable).alpha = alpha0To100*255/100
+        }
+    }
+}
+
+@BindingAdapter("cardBgColorAlpha")
+fun CardView.setCardBgColorAlpha(alpha0To100: Int?) {
+    alpha0To100?.let {
+        setCardBackgroundColor(cardBackgroundColor.withAlpha(alpha0To100*255/100))
+    }
+}
