5772a052c40bdc49c40779c12725cc99e06dcbf2
depayse <cbs5295@gmail.com>
2022-11-27 13:31:27 +0900

    [FEAT] BaseBottomSheetDialogFragment.kt 작성

---
+++ app/src/main/java/com/vside/app/util/base/BaseBottomSheetDialogFragment.kt
@@ -0,0 +1,27 @@
+package com.vside.app.util.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BaseBottomSheetDialogFragment<T: ViewDataBinding, VM: BaseViewModel>: BottomSheetDialogFragment() {
+    lateinit var viewDataBinding: T; private set
+
+    abstract val layoutResId: Int
+    abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        viewDataBinding.lifecycleOwner = viewLifecycleOwner
+
+        return viewDataBinding.root
+    }
+}
