54e89517016723eab28553d1694914a090a63ec3
depayse <cbs5295@gmail.com>
2022-12-14 21:11:43 +0900

    [FEAT] 홈 ViewModel 영역 Api 연동

--- app/src/main/java/com/vside/app/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/vside/app/feature/home/HomeViewModel.kt
@@ -1,6 +1,25 @@
 package com.vside.app.feature.home
 
+import com.vside.app.feature.home.repo.HomeRepository
 import com.vside.app.util.base.BaseViewModel
+import com.vside.app.util.common.handleApiResponse
+import kotlinx.coroutines.flow.collect
 
-class HomeViewModel: BaseViewModel() {
+class HomeViewModel(private val homeRepository: HomeRepository): BaseViewModel() {
+    suspend fun getHomeContentList(onGetSuccess: () -> Unit, onGetFail: () -> Unit) {
+        homeRepository.getHomeContentList(tokenBearer)
+            .collect { response ->
+                handleApiResponse(
+                    response = response,
+                    onSuccess = {
+                        onGetSuccess()
+                    },
+                    onError = {
+                        onGetFail()
+                    }, onException = {
+                        onGetFail()
+                    }
+                )
+            }
+    }
 }
