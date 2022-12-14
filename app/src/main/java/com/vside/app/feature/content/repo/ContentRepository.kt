e8eed5542dc915186e0ef5edc4f565b6ac5e2171
depayse <cbs5295@gmail.com>
2022-12-14 21:26:24 +0900

    [FEAT] 컨텐츠 Repository 영역 Api 연동

--- app/src/main/java/com/vside/app/feature/content/repo/ContentRepository.kt
+++ app/src/main/java/com/vside/app/feature/content/repo/ContentRepository.kt
@@ -1,6 +1,11 @@
 package com.vside.app.feature.content.repo
 
 import com.vside.app.feature.content.service.ContentService
+import com.vside.app.util.common.flowApiResponse
+import java.math.BigInteger
 
 class ContentRepository(private val contentService: ContentService) {
+    suspend fun getContentDetail(tokenBearer: String, contentId: BigInteger) = flowApiResponse(contentService.getContentDetail(tokenBearer, contentId))
+
+    suspend fun toggleScrapContent(tokenBearer: String, contentId: BigInteger) = flowApiResponse(contentService.getContentDetail(tokenBearer, contentId))
 }
