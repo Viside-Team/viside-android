58121b43d0e8162fab5c100a9fe6240afcb351e7
depayse <cbs5295@gmail.com>
2023-08-15 07:55:32 +0900

    [CHORE] AuthInterceptor 주석 작성

--- app/src/main/java/com/vside/app/util/auth/AuthInterceptor.kt
+++ app/src/main/java/com/vside/app/util/auth/AuthInterceptor.kt
@@ -20,6 +20,7 @@ class AuthInterceptor(private val context: Context): Interceptor {
             try {
                 val bodyStr = response.peekBody(Long.MAX_VALUE).string()
                 val signInResponse = Gson().fromJson(bodyStr, SignInResponse::class.java)
+                // AccessToken 만 내려왔을 때는 AccessToken 만을 업데이트 후 같은 요청을 보냄
                 if(signInResponse.jwtAccessBearer != null && signInResponse.refreshToken == null) {
                     updateJwtAccessBearer(context, signInResponse.jwtAccessBearer)
                     // AccessToken 업데이트 이후 재요청
@@ -33,6 +34,11 @@ class AuthInterceptor(private val context: Context): Interceptor {
         return response
     }
 
+    /**
+     * 인증이 필요한 요청인지 판단
+     *
+     * @param originalRequest 인증이 필요한 요청인지 판단할 Http 요청
+     */
     private fun isAuthNeeded(originalRequest: Request): Boolean {
         with(originalRequest.url.encodedPath) {
             if(contains("signin") || contains("login") || contains("nameCheck")) return false
@@ -40,6 +46,9 @@ class AuthInterceptor(private val context: Context): Interceptor {
         return true
     }
 
+    /**
+     * 인증이 필요한 요청일 때 AccessToken 과 RefreshToken 을 Header 에 붙여줌
+     */
     private fun buildAuthRequest(context: Context, originalRequest: Request) =
         if(isAuthNeeded(originalRequest)) originalRequest.newBuilder()
             .addHeader("Authorization", SharedPrefManager.getString(context) { JWT_ACCESS_BEARER })
